67be23fa8ea1222f7e7d9af7b686a2a4e508fef7
jp5485 <78568307+jp5485@users.noreply.github.com>
2021-02-07 15:33:37 -0500

    website live with js query line

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $('.Space').on('click', function() {
-	$('#welcome-message').css({'background-image': 'url(https://i.pinimg.com/originals/b9/fc/97/b9fc9753e973c0d4078a01aaab663420.png)', 'background-repeat':'repeat', 'background-size' : 'cover'})
+	$('#welcome-message').css({'background-image': 'url("https://i.pinimg.com/originals/b9/fc/97/b9fc9753e973c0d4078a01aaab663420.png")', 'background-repeat':'repeat', 'background-size' : 'cover'})
 })
 
 
@@ -25,3 +25,4 @@ $('.Mind').on('click', function() {
 $('.Time').on('click', function() {
 	$('#welcome-message').css({'background-image': 'url(https://img.wattpad.com/b415447b800d87381d96e80f6162b166e7fe1767/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f6d4d68522d49637261374e5664513d3d2d3736343632333631362e313562356237343566396137363265613932313638393431373631372e676966?s=fit&w=720&h=720)', 'background-repeat':'no-repeat', 'background-size':'cover'})
 })
+console.log("Must be something else because JQuery is weorking")
