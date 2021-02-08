a427ba8d38531672a1dd5716599a59d93cd8f34a
jp5485 <78568307+jp5485@users.noreply.github.com>
2021-02-07 22:06:57 -0500

    FInal Version

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+$('.infinity-stones').on('mouseover', function() {
+	$('#welcome-message').css({'background-image': 'url("https://thumbs.gfycat.com/LargeWeakAmethystinepython-max-1mb.gif")',
+  'background-repeat':'no-repeat',
+  'background-size' : 'cover',
+  'opacity':'0.8'
+})
+})
+
+
+
 $('.Space').on('click', function() {
 	$('#welcome-message').css({'background-image': 'url("https://img.wattpad.com/1cf4d0a1271e70aa2991de265476de10c9ba7fb6/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f7147684c58574c654f4e685270773d3d2d3736363739323333332e313562373266646364336431653830343734363830313232323234312e676966?s=fit&w=720&h=720")', 'background-repeat':'repeat', 'background-size' : 'cover'})
 })
@@ -25,4 +35,7 @@ $('.Mind').on('click', function() {
 $('.Time').on('click', function() {
 	$('#welcome-message').css({'background-image': 'url(https://img.wattpad.com/b415447b800d87381d96e80f6162b166e7fe1767/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f6d4d68522d49637261374e5664513d3d2d3736343632333631362e313562356237343566396137363265613932313638393431373631372e676966?s=fit&w=720&h=720)', 'background-repeat':'no-repeat', 'background-size':'cover'})
 })
-console.log("Must be something else because JQuery is weorking")
+
+$('.steve').on('click', function() {
+	$('#welcome-message').css({'background-image': 'url(https://i.pinimg.com/originals/97/16/27/971627fa7739b2c32bb0376b5c579a15.jpg)', 'background-repeat':'no-repeat', 'background-size':'cover'})
+})
